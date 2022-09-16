ffcffae957733b5e12412e0f6034889eaa5b6184
Noah <noah.lissner@gmail.com>
2022-09-16 09:16:51 +0200

    refactor: cleanup console.log

--- features/auth/authService.js
+++ features/auth/authService.js
@@ -1,43 +1,41 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API_URL =
-	'https://expense-app-backend-production.up.railway.app/api/users/';
+  "https://expense-app-backend-production.up.railway.app/api/users/";
 
 const register = async (userData) => {
-	console.log(userData);
-	const res = await axios.post(API_URL + 'register', userData);
-	// console.log(res);
-	if (res.data) {
-		const value = JSON.stringify(res.data);
-		await AsyncStorage.setItem('user', value);
-	}
+  const res = await axios.post(API_URL + "register", userData);
+  if (res.data) {
+    const value = JSON.stringify(res.data);
+    await AsyncStorage.setItem("user", value);
+  }
 
-	return res.data;
+  return res.data;
 };
 
 const login = async (userData) => {
-	const res = await axios.post(API_URL + 'login', userData);
-	if (res.data) {
-		const value = JSON.stringify(res.data);
-		await AsyncStorage.setItem('user', value);
-	}
+  const res = await axios.post(API_URL + "login", userData);
+  if (res.data) {
+    const value = JSON.stringify(res.data);
+    await AsyncStorage.setItem("user", value);
+  }
 
-	return res.data;
+  return res.data;
 };
 
 const logout = async () => {
-	try {
-		await AsyncStorage.removeItem('user');
-	} catch (e) {
-		console.error(e);
-	}
+  try {
+    await AsyncStorage.removeItem("user");
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 const authService = {
-	register,
-	login,
-	logout,
+  register,
+  login,
+  logout,
 };
 
 export default authService;
