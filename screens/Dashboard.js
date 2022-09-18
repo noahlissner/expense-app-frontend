10118a416b10e7a15b803324d667f3627cf431d3
noahlissner <noah.lissner@gmail.com>
2022-09-18 22:21:35 +0200

    fix: import useEffect

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -8,7 +8,7 @@ import {
   RefreshControl,
   ScrollView,
 } from "react-native";
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import GroupCard from "../components/GroupCard";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -20,12 +20,12 @@ const wait = (timeout) => {
 };
 
 const Dashboard = ({ navigation }) => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const [refreshing, setRefreshing] = React.useState(false);
-  
-  	useEffect(() => {
-		dispatch(fetchGroups());
-	}, []);
+
+  useEffect(() => {
+    dispatch(fetchGroups());
+  }, []);
 
   const onRefresh = React.useCallback(() => {
     setRefreshing(true);
@@ -79,25 +79,25 @@ const Dashboard = ({ navigation }) => {
 export default Dashboard;
 
 const styles = StyleSheet.create({
-	wrapper: {
-		marginRight: 30,
-		marginLeft: 30,
-		height: '100%',
-	},
-	cardWrapepr: {
-		marginBottom: 10,
-	},
+  wrapper: {
+    marginRight: 30,
+    marginLeft: 30,
+    height: "100%",
+  },
+  cardWrapepr: {
+    marginBottom: 10,
+  },
 
-	// Button to add group
-	addBtn: {
-		position: 'absolute',
-		bottom: 0,
-		left: '50%',
-		transform: [{ translateX: -32 }],
-		height: 65,
-		width: 65,
-		borderRadius: 999,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  // Button to add group
+  addBtn: {
+    position: "absolute",
+    bottom: 0,
+    left: "50%",
+    transform: [{ translateX: -32 }],
+    height: 65,
+    width: 65,
+    borderRadius: 999,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
