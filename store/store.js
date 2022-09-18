4f7636b5729253d0c6dd6e038e9c6d50815e993c
dough <viktor.melin@me.com>
2022-09-18 22:25:43 +0200

    fix(groups): added group reducer to store

--- store/store.js
+++ store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer, { initUser } from '../features/auth/authSlice';
+import groupReducer from '../features/groups/groupSlice';
 
 const store = configureStore({
 	reducer: {
 		auth: authReducer,
+		groups: groupReducer,
 	},
 });
 
