372652e5b131a7824cbaab3108f38875380b9324
dough <viktor.melin@me.com>
2022-09-18 21:55:55 +0200

    feat(groups): added group services + slice

---
+++ features/groups/groupSlice.js
@@ -0,0 +1,70 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import groupService from './groupService';
+
+const initialState = {
+	groups: null,
+	isError: false,
+	isLoading: false,
+	isSuccess: false,
+	message: '',
+};
+
+export const fetchGroups = createAsyncThunk(
+	'group/fetchall',
+	async (_, thunkAPI) => {
+		try {
+			return await groupService.fetchGroups();
+		} catch (err) {
+			const message =
+				(err.response && err.response.data && err.response.data.message) ||
+				err.message ||
+				err.toString();
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
+export const fetchGroup = createAsyncThunk(
+	'group/fetch',
+	async (data, thunkAPI) => {
+		try {
+			return await groupService.fetchGroup(data);
+		} catch (err) {
+			const message =
+				(err.response && err.response.data && err.response.data.message) ||
+				err.message ||
+				err.toString();
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
+export const groupSlice = createSlice({
+	name: 'groups',
+	initialState,
+	reducers: {
+		reset: (state) => {
+			state.isLoading = false;
+			state.isSuccess = false;
+			state.isError = false;
+			state.message = '';
+		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(fetchGroups.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(fetchGroups.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.groups = action.payload;
+			})
+			.addCase(fetchGroups.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+				state.groups = null;
+			});
+	},
+});
